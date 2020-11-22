e923fe1f1861f1ca5942f3074102caab6b3406c4
tianfangma <73506573+tianfangma@users.noreply.github.com>
2020-11-22 19:25:08 +0800

    Create 1.cpp

---
+++ problem/1-100/1.cpp
@@ -0,0 +1,17 @@
+//hash map
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> hashmap;
+        for(int i=0;i<nums.size();++i)
+        {
+            auto it=hashmap.find(target-nums[i]);
+            if(it!=hashmap.end())
+            {
+                return {it->second,i};
+            }
+            hashmap[nums[i]]=i;
+        }
+        return {0};
+    }
+};
