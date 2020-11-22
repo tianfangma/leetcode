1bbf4e42982eceae448d5f47a80192776a60bfe2
tianfangma <73506573+tianfangma@users.noreply.github.com>
2020-11-22 19:52:30 +0800

    Create 15.cpp

---
+++ problem/1-100/15.cpp
@@ -0,0 +1,38 @@
+//enumerate
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>res;
+        int n=nums.size();
+        for(int first = 0;first<n;++first)
+        {
+            if(first>0&&nums[first]==nums[first-1])
+            {
+                continue;
+            }
+            int third = n-1;
+            int target = -nums[first];
+            for(int second = first+1;second<n;++second)
+            {
+                if(second>first+1&&nums[second]==nums[second-1])
+                {
+                    continue;
+                }
+                while(second<third&&nums[second]+nums[third]>target)
+                {
+                    third--;
+                }
+                if(second==third)
+                {
+                    break;
+                }
+                if(nums[second]+nums[third]==target)
+                {
+                    res.push_back({nums[first],nums[second],nums[third]});
+                }
+            }
+        }
+        return res;
+    }
+};
