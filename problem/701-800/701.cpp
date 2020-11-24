5cd1096c0a316fc58b19a1bba0dbc39abc603012
tianfangma <73506573+tianfangma@users.noreply.github.com>
2020-11-24 14:23:37 +0800

    Update 701.cpp

--- problem/701-800/701.cpp
+++ problem/701-800/701.cpp
@@ -53,3 +53,54 @@ public:
         return root;
     }
 };
+
+//leetcode solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(root==nullptr)
+        {
+            return new TreeNode(val);
+        }
+        TreeNode*pos=root;
+        while(pos!=nullptr)
+        {
+            if(pos->val>val)
+            {
+                if(pos->left==nullptr)
+                {
+                    pos->left=new TreeNode(val);
+                    break;
+                }
+                else
+                {
+                    pos=pos->left;
+                }
+            }
+            else
+            {
+                if(pos->right==nullptr)
+                {
+                    pos->right=new TreeNode(val);
+                    break;
+                }
+                else
+                {
+                    pos=pos->right;
+                }
+            }
+        }
+        return root;
+    }
+};
